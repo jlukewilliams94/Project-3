1437713827f2e735d9fe8b802104ec657ed8c0e0
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-06-19 13:39:02 -0400

    added logo

--- project/src/pages/Age/index.js
+++ project/src/pages/Age/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import Beer from './beer.mp4'
 import 'materialize-css';
 import { Button, Card, Row, Col } from 'react-materialize';
+import Logo from './logo.png'
 
 
 function Age() {
@@ -13,17 +14,20 @@ function Age() {
                 <div className="col s12 m7">
                     <div className="card">
                         <div className="card-image">
-                            <video className="materialboxed data-caption" width="650" autoPlay muted loop id="myVideo"
-                            //width="650" height="100"
+                            <video className="materialboxed data-caption" width="100%" autoPlay muted loop id="myVideo"
+                                //width="650" height="100"
                                 src={Beer} type="video/mp4" />
-                             <div className="content">
-                            <span className="card-title caption center-align">Are you over 21?
-                            <div>
-                             <button className="btn waves-effect waves-light btn-large" href="/">Yes</button>
-                             <button className="btn waves-effect waves-light btn-large" href="/">No</button>
-                             </div>
+                                
+                                    <img className="logo" src={Logo} width="100"/>
+                                
+                            <div className="content">
+                                <span className="card-title caption center-align">Are you 21 or older?
+                                     <div>
+                                        <button className="btn waves-effect waves-light btn-large" href="/">Yes</button>
+                                        <button className="btn waves-effect waves-light btn-large" href="/">No</button>
+                                    </div>
                                 </span>
-                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
