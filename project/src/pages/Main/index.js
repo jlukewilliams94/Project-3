6f1d88d70dffd24a3a2a1ce886ec977d9f4eb4f4
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-06-20 10:50:32 -0400

    added image

--- project/src/pages/Main/index.js
+++ project/src/pages/Main/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+//import { Card, Row, Col, Icon, CardTitle } from 'react-materialize';
+import Charlotte from './Charlotte.jpg'
 import './style.css'
 
 function Main() {
   return (
     <div>
       <Navbar />
+        <div>
+          <img src={Charlotte} alt="skyline" className="image" />
+        </div>
+  
     </div>
   )
 }
