2ebde95aecf638b9c0b9eaa2f0579d6234f78d03
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-06-20 12:59:24 -0400

    changed navbar and nav logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import Logo from '../../pages/Age/logo.png';
+//import Logo from '../../pages/Age/logo.png';
 import { Link } from 'react-router-dom';
 import './style.css';
+import NavLogo from './navlogo.png';
 //import { Navbar } from 'react-materialize';
 
 function Navbar() {
 
     return (
         <div>
-            <nav className="navbar" style={{ height: "200px" }}>
+            <nav className="navbar" style={{height: "70px"}}>
                 <div className="nav-wrapper">
-                    <a href="/main" className="brand-logo center">
-                        <img src={Logo} alt="logo" />
-                    </a>
-                    <ul className="left hide-on-med-and-down menu ">
-                        {(<Link to="/main" className="list">Home </Link>)}
-                        {(<Link to="/admin" className="list">Admin Login </Link>)}
+                    
+                    <span className="left hide-on-med-and-down menu ">
+                        {(<Link to="/main" className="list"> &nbsp;&nbsp;&nbsp; Home &nbsp;&nbsp;&nbsp;&nbsp;</Link>)}
+                        {(<Link to="/admin" className="list">Admin Login &nbsp;&nbsp;&nbsp;&nbsp; </Link>)}
                         {(<Link to="/shop" className="list">Shop </Link>)}
-                    </ul>
-                    <ul className="right hide-on-med-and-down menu ">
-                        {(<Link to="/cart" className="list"><i className="fa fa-shopping-cart"></i> </Link>)}
-                        {(<Link to="/community" className="list">Community </Link>)}
-                        {(<Link to="/contact" className="list">Contact Us </Link>)}
-                    </ul>
+                    </span>
+                    <a href="/main" className="brand-logo center">
+                        <img src={NavLogo} alt="logo" />
+                    </a>
+                    <span className="right hide-on-med-and-down menu ">
+                        {(<Link to="/cart" className="list"><i className="fa fa-shopping-cart"></i>&nbsp; </Link>)}
+                        {(<Link to="/community" className="list"> &nbsp;&nbsp;&nbsp;&nbsp;Community </Link>)}
+                        {(<Link to="/contact" className="list"> &nbsp;&nbsp;&nbsp;&nbsp;Contact Us &nbsp;&nbsp;&nbsp; </Link>)}
+                    </span>
                 </div>
             </nav>
         </div>
