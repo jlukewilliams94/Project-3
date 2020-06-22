539f8862b92b0a4a985d00a82b7f555d8ac1b7a4
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-06-22 17:49:06 -0400

    changed navbar font and added brewery pictures

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
-//import { Card, Row, Col, Icon, CardTitle } from 'react-materialize';
+import { Row } from 'react-materialize';
 import './style.css'
 //import { Link } from 'react-router-dom';
 import BeerWall from './beerwall.jpg'
+import Birdsong from '../../components/Images/birdsong.png';
+import RC from '../../components/Images/ResidentCulture.png';
+import NoDa from '../../components/Images/NoDa.jpg';
+import Legion from '../../components/Images/Legion.jpg';
 
 function Main() {
   return (
@@ -11,7 +15,13 @@ function Main() {
       <Navbar />
         <div>
           {/* <h1>{(<Link to='/shop' className="local">BUY QUEEN CITY BEER</Link>)}</h1> */}
-          <img src={BeerWall} alt="beer" className="beer" />
+          <img src={BeerWall} alt="beer" className="beer"/> 
+          <Row>
+          <img src={Birdsong} alt="birdsong" className="localBreweries"/>
+          <img src={RC} alt="RC" className="localBreweries"/>
+          <img src={NoDa} alt="NoDa" className="localBreweries"/>
+          <img src={Legion} alt="Legion" className="localBreweries"/>
+          </Row>
         </div>
   
     </div>
