0983af822e7957a93094b83e1e6efcedd91d18c9
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-06-22 17:23:30 -0400

    front end admin login can now redirect

---
+++ src/lib/API.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default {
+    Users:{
+        login: function (email, password){
+           // return axios.post('/api/users/login', {email, password})
+           if(password === 'test'){
+               return {email, password}
+           }
+           return "504"
+        }
+    }
+}
