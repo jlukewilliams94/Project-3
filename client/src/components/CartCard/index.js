7d2e172c16e7dcee8a453c6aa878feb5b5ed8471
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-07-06 15:29:20 -0400

    added checkout page

--- client/src/components/CartCard/index.js
+++ client/src/components/CartCard/index.js
@@ -1,41 +1,46 @@
-import React from 'react'
-import { Card, Col, CardTitle, Row, Button } from 'react-materialize';
+import React, { useState, useEffect } from 'react'
+import { Card, CardTitle, Col } from 'react-materialize';
 import './cartCard.css';
 // import { PayPalButton } from "react-paypal-button-v2";
 // import Payment from '../../components/Payment'
 
-function Cart() {
-    const beerName = localStorage.getItem('item')
-    const beerImage = localStorage.getItem('picture')
-    const beerPrice = localStorage.getItem('price')
+const Cart = props => {
+
+    const [quantity, setQuantity] = useState(1)
+    useEffect(() => {
+        props.math(props.beerInfo.price)
+    }, [])
+    const add = () => {
+        setQuantity(curr => curr + 1)
+        props.math(props.beerInfo.price)
+    }
+    const subtract = () => {
+        if (quantity !== 0) {
+            setQuantity(curr => curr - 1)
+            props.math(-props.beerInfo.price)
+        }
+    }
+
     return (
-        <div>
-            <Row>
-                <Col
-                    xl={2}
-                    l={3}
-                    m={4}
-                    s={12}
-                >
-                    <Card className="cartCard"
-                        header={<CardTitle image={beerImage}></CardTitle>}
-                    >
-                        <b>Beer:</b> {beerName}<br />
-                        <b>Price:</b> {beerPrice}<br />
-                        <b>Quantity: </b>
-                    </Card>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Card>
-                        <b>Total:</b>{beerPrice}<br />
-                        <Button>Pay Now</Button>
-                        {/* <Payment /> */}
-                    </Card>
-                </Col>
-            </Row>
-        </div>
+        <Col
+            xl={3}
+            l={4}
+            m={5}
+            s={12}
+        >
+
+            <Card
+                header={<CardTitle image={props.beerInfo.picture}></CardTitle>}
+            >
+                <div className="cartCard">
+                    <b>Beer:</b> {props.beerInfo.item}<br />
+                    <b>Price:</b> {props.beerInfo.price}<br />
+                    <b>Quantity:</b>
+                    <span onClick={add}>&nbsp;<i className="fa fa-lg quantity fa-plus-square" aria-hidden="true"></i>&nbsp;</span>{quantity}<span onClick={subtract}>&nbsp;<i className="fa fa-lg quantity fa-minus-square" aria-hidden="true"></i>&nbsp;</span>
+                </div>
+            </Card>
+        </Col>
+
     )
 }
 
--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css'
 import CartCard from '../../components/CartCard'
+import { Card, Col, Row, Button } from 'react-materialize';
+import Checkout from '../Checkout'
+import { Link } from 'react-router-dom'
+
 
 function Cart() {
-    return(
+
+    const [items, setItems] = useState([])
+    const [total, setTotal] = useState(0)
+
+    useEffect(() => {
+        const itemList = JSON.parse(localStorage.getItem('cart'))
+        setItems(itemList)
+    }, [])
+
+    const sum = (price) => {
+        setTotal(currTotal => currTotal + price)
+    }
+
+    return (
         <div>
-            <h3 className="cart">great choice.</h3>
-            <CartCard/>
-        </div>
+
+            {(items.length > 0) ? (
+                items.map(item => {
+                    return (
+                        <div>
+                            <Row>
+                                <h3 className="cart">great choice.</h3>
+                            </Row>
+
+                            <Row>
+                                <CartCard key={item}
+                                    beerInfo={item}
+                                    math={sum}
+                                />
+                            </Row>
+                        </div>
+                    )
+                })
+            ) : (
+                    <h2 className="tagline">cart is empty <i className="fa fa-frown-o fa-2x" aria-hidden="true"></i></h2>
+                )}
+            <Row>
+                <Col>
+                    <Card>
+                        <div className="cartCard">
+                            <b>Total:</b> {total.toFixed(2)}<br />
+                        </div>
+                        <Button>{(<Link to="/checkout" className="checkoutBtn">Checkout</Link>)}</Button>
+                </Card>
+            </Col>
+
+        </Row>
+   
+        </div >
     )
 }
 
