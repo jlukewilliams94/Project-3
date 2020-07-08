df60b86842a8a9347ff0abb29d803d2281f999fc
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-07-08 19:35:47 -0400

    updated css opacity to important

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
-//import Navbar from '../../components/Navbar';
-// import { Row } from 'react-materialize';
 import './style.css'
-//import { Link } from 'react-router-dom';
-//import BeerWall from './beerwall.jpg'
-// import Birdsong from '../../components/Images/birdsong.png';
-// import RC from '../../components/Images/ResidentCulture.png';
-// import NoDa from '../../components/Images/NoDa.jpg';
-// import Legion from '../../components/Images/Legion.jpg';
-//import Footer from '../../components/Footer'
 import BeerMap from '../../components/Images/beerMap1.png'
 import Charlotte from '../../components/Images/Charlotte.jpg'
 
@@ -20,13 +11,6 @@ function Main() {
         <div className="beer2">
           <img src={BeerMap} alt="beer" className="beer" />
         </div>
-
-        {/* <Row>
-          <img src={Birdsong} alt="birdsong" className="localBreweries"/>
-          <img src={RC} alt="RC" className="localBreweries"/>
-          <img src={NoDa} alt="NoDa" className="localBreweries"/>
-          <img src={Legion} alt="Legion" className="localBreweries"/>
-          </Row> */}
     </div>
   )
 }
